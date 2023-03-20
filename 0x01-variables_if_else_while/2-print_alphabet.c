6a5bf0bb75863504b8ebe1cbeaeefa46b24ffa89
atemogabriella <atemogabriella0@gmail.com>
2023-03-20 13:40:52 -0700

    Task 1. The last digit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char n;
+
+	for (n = 'a'; 'z'; n++)
+		putchar(n);
+
+	putchar('\n');
+
+	return (0);
+}

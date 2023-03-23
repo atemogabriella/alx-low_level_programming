c78de9d2211001dae7d881f379f10ed10316515a
atemogabriella <atemogabriella0@gmail.com>
2023-03-23 13:45:02 -0700

    Task 0. Multiple mains

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,32 +1,15 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
+#include "main.h"
 
 /**
-* main - Entry point
+*positive_or_negative - Checks for positive or negative numbers
+*@i: The number to be checked
 * Return: Always 0 (Success)
 */
-int main(void)
+
+void positive_or_negative(int i)
 {
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-
-	if (n > 0)
-	{
-		printf("%d is positive\n", n);
-	}
-
-	if (n < 0)
-	{
-		printf("%d is negative\n", n);
-	}
-
-	else if (n == 0)
-	{
-		printf("%d is zero\n", n);
-	}
-
-	return (0);
+	if (i < 0)
+		printf("%d is negative\n", i);
+	else
+		printf("%d is positive\n", i);
 }

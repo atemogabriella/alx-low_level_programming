dc842c6ea78f8aab9d173f7978b899a7048049bb
atemogabriella <atemogabriella0@gmail.com>
2023-03-24 07:46:20 -0700

    Task 0.isupper

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,14 @@
+int _putchar(char c);
+int _isupper(int x);
+int _isdigit(int x);
+void print_triangle(int size);
+int main(void);
+void print_number(int n);
+int mul(int x, int y);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+int main(void);

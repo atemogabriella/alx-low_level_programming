19a3f12bf7bbfbdb3f48eb12a7326c01c73b9eb3
atemogabriella <atemogabriella0@gmail.com>
2023-04-03 18:00:43 -0700

    Task 2. strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,7 +10,7 @@ char *_strchr(char *s, char c)
 {
 	while (*s != '\0')
 	{
-		if(*s == c)
+		if (*s == c)
 		{
 			return (s);
 		}
@@ -20,5 +20,5 @@ char *_strchr(char *s, char c)
 	if (*s == c)
 		return (s);
 	else
-		return(0);
+		return (0);
 }

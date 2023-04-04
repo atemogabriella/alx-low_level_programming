a1f283746d20ceaabd3724d3cad175a0ff74a5f0
atemogabriella <atemogabriella0@gmail.com>
2023-04-04 15:55:32 -0700

    5. strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strstr - string occurrence area
+ * @haystack: memory to be filled
+ *@needle: character to be copied
+ *Return: filled memory area
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int m;
+	while (*haystack)
+	{
+		for(m = 0; needle[m]; m++)
+		{
+			if (*haystack == needle['\0'])
+			return (haystack);
+		}
+		haystack++;
+	}
+	return (haystack);
+}
+

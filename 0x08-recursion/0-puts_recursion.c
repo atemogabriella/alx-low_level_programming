fde9929051043275a2ef91bed3933b5bfa94c2ea
atemogabriella <atemogabriella0@gmail.com>
2023-04-05 15:54:41 -0700

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,26 +7,11 @@
  */
 void _puts_recursion(char *s)
 {
-	putchar(*s);
-	putchar('P');
-	putchar('u');
-	putchar('t');
-	putchar('s');
-	putchar(' ');
-	putchar('w');
-	putchar('i');
-	putchar('t');
-	putchar('h');
-	putchar(' ');
-	putchar('r');
-	putchar('e');
-	putchar('c');
-	putchar('u');
-	putchar('r');
-	putchar('s');
-	putchar('i');
-	putchar('o');
-	putchar('n');
-	putchar('\n');
-	
+	if (*s)
+	{
+		putchar(*s);
+		_puts_recursion(s + 1);
+		putchar('\n');
+	}
+
 }

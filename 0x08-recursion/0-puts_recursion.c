2ed6b908fdd8cfcccad1ad4bede97d3a66eb1f61
atemogabriella <atemogabriella0@gmail.com>
2023-04-05 15:19:14 -0700

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts_recursion - print newline area
+ * @s: string to be printed
+ *Return : string printed
+ */
+void _puts_recursion(char *s)
+{
+	putchar(*s);
+	putchar('P');
+	putchar('u');
+	putchar('t');
+	putchar('s');
+	putchar(' ');
+	putchar('w');
+	putchar('i');
+	putchar('t');
+	putchar('h');
+	putchar(' ');
+	putchar('r');
+	putchar('e');
+	putchar('c');
+	putchar('u');
+	putchar('r');
+	putchar('s');
+	putchar('i');
+	putchar('o');
+	putchar('n');
+	putchar('\n');
+	
+}

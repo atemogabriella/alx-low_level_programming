0f3a582aec673b6e56d6f20e5e2c9482124ab2dd
atemogabriella <atemogabriella0@gmail.com>
2023-04-05 18:36:30 -0700

    3. You mustn't be afraid to dream a little bigger, darling

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * factorial - number factorial area
+ *@n: variable in which the factorial of itself is to be found
+ *Return: factorial of n
+ */
+int factorial(int n)
+{
+	if (n == 1)
+	{
+		return (1);
+	}
+	else if (n < 1)
+	{
+		return (- 1);
+	}
+	else
+		 return (n * factorial(n - 1));
+	return (n);
+}

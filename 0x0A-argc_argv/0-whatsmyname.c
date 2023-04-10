0c9a1decc69bc47cb41b4aa8c167ec5fc10550fd
atemogabriella <atemogabriella0@gmail.com>
2023-04-10 13:29:22 -0700

    0. It ain't what they call you, it's what you answer to

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - program execution area
+ * void - area where no value is returned
+ * Return: 0
+ */
+int main(void)
+{
+	printf("mynameis\n");
+	return (0);
+}
+

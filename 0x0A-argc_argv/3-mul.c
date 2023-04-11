69f0e34ad025749668ab94befee56668c9b33ee8
atemogabriella <atemogabriella0@gmail.com>
2023-04-10 18:37:15 -0700

    3. Neither irony nor sarcasm is argument

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,16 +1,21 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
  * main - program execution area
  * @argc: area where things in command line are counted
  * @argv: character pointers area
  * Return: 0
  */
-int main(int argc, char **argv)
+int main(int argc, char __attribute__((__unused__)) **argv)
 {
 	int g, k;
 
 	for (g = 0; g < argc; g++)
-	
-		k = k * argv[g]
+	{
+		k = k * g;
+	}
+	printf("%d\n", k);
+	return (k);
+}
 

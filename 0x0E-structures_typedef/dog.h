fe2770f0c0f563dd5ff529831c2c4c2884f20444
atemogabriella <atemogabriella0@gmail.com>
2023-04-17 12:40:55 -0700

    0. Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,20 @@
 #ifndef DOG_H
 #define DOG_H
-
+/**
+ * struct dog - dog structures and characteristics
+ * @name: dog name
+ * @age: dog age
+ * @owner: person owning the dog
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+/**
+ * dog_t - definition for dog structure
+ */
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);

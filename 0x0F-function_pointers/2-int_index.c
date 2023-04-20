44b3b90bfde50321a7240bcc3f016101a56cde31
atemogabriella <atemogabriella0@gmail.com>
2023-04-19 17:39:44 -0700

    2. To hell with circumstances; I create opportunities

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+/**
+ * int_index - returns index if condition is true
+ * @array: array
+ * @size: number of elements in an array
+ * @cmp: pointer in function comparing values
+ * Return: 0 (Success)
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int g;
+
+	if (array != NULL && cmp != NULL && size <= 0)
+	{
+		return (-1);
+	}
+
+	for (g = 0; g < size; g++)
+	{
+		if (cmp(array[g]))
+		{
+			return (g);
+		}
+	}
+	return (-1);
+}
+

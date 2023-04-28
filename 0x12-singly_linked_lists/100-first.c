e7c1af8df240390066bd430c37979a24b10c9683
atemogabriella <atemogabriella0@gmail.com>
2023-04-27 17:25:13 -0700

    5. The Hare and the Tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * first_sentc - prints the first sentence
+ * Description: This function prints first message before main function
+ */
+void first_sentc(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}

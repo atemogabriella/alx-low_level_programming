ab159c8ba05db9ee1c30f7f9b44e7b53d302ca1a
atemogabriella <atemogabriella0@gmail.com>
2023-05-09 15:23:31 -0700

    1. Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,42 +10,29 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int f, q, z;
+	int f, q, z = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	f = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR, S_IWUSR);
+	f = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (f == -1)
 		return (-1);
 
-	if (text_content == NULL)
+	if (text_content != NULL)
 	{
-		close(f);
-		return (1);
-	}
-
-	z = 0;
-	while (text_content[z] != '\0')
+		while (text_content[z])
 		z++;
 
-	q = write(f, text_content, z);
-
-	if (q == -1)
-	{
-		close(f);
-		return (-1);
+		q = write(f, text_content, z);
+		
+		if (q == -1 || q < z)
+		{
+			close(f);
+			return (-1);
+		}
 	}
-
-	if (q != z)
-	{
-		close(f);
-		return (-1);
-	}
-
 	close(f);
 	return (1);
 }
-
-
 
